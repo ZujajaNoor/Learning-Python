5690eae6cde89e030c059bcbf1d7db3ff2f64daa
ZujajaNoor <zujajahnoorghulamm@gmail.com>
2022-01-02 20:14:22 +0500

    Done with all tasks

---
+++ Python Assignment-1.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ASSIGNMENT
+
+# ## Task 1
+
+# In[4]:
+
+
+poem = """Twinkle,twinkle,liittle star,
+                How i wonder what you are!
+                        Up above the world so high,
+                        like a diamond in the sky.
+Twinkle,twinkle,little star,
+                How i wonder what you are"""
+print(poem)    
+
+
+# ## Task 2
+
+# In[3]:
+
+
+pip --version
+
+
+# ## Task 3
+
+# In[7]:
+
+
+import datetime
+x = datetime.datetime.now()
+print(x)
+
+
+# ## Task 4
+
+# In[16]:
+
+
+PI = 3.14
+r = float(input("Enter the value of radius:"))
+Area = PI*r**2;
+print(Area)
+
+
+# ## Task 5
+
+# In[12]:
+
+
+A1 = input("Enter the First name:")
+A2 = input("Enter the Last name:")
+print (A2+ " " +A1)
+
+
+# ## Task 6
+
+# In[15]:
+
+
+number1 = input("Enter the value of first number:")
+number2 = input("Enter the value of first number:")
+addition = int(number1) + int(number2)
+print (addition)
+
+
+# # I have done sir.
